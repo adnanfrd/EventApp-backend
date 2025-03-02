376f279f54731ebf231c6e2ba03c741059086f45
adnanfrd <adnanmadhar74@gmail.com>
2025-03-02 23:13:27 +0500

    backend

--- index.js
+++ index.js
@@ -9,16 +9,20 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const dbURL =  process.env.MONGO_URI;
+const dbURL = process.env.MONGO_URI;
+
+app.get('/', async (req, res) => {
+  res.send("Hello, This is backend.");
+});
 
 app.use(express.json());
 app.use(cors());
-app.use('/api/user', userRoutes)
+app.use('/api/user', userRoutes);
 
 mongoose.connect(dbURL)
   .then(() => console.log('✅ MongoDB connected Successfully!'))
   .catch(err => console.log('MongoDB connection error:', err));
 
-app.listen(PORT, async () => {
+app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
